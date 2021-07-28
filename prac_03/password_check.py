9aa30b8befdae417ba162949de53969677daa7c7
Josiah Teh <lingqiangjosiah.teh@my.jcu.edu.au>
2021-07-28 15:08:40 +0800

    Refactor the program to use functions

--- prac_03/password_check.py
+++ prac_03/password_check.py
@@ -1,8 +1,24 @@
 MIN_LENGTH = 2
 MAX_LENGTH = 5
 
-password = input('Enter a password between {} to {} characters: '.format(MIN_LENGTH, MAX_LENGTH))
-while len(password) < MIN_LENGTH or len(password) > MAX_LENGTH:
-    print('Error: Password Does Not Meet Required Length')
+
+def main():
+    user_input = get_valid_password()
+    print_asterisk(user_input)
+
+
+def get_valid_password():
+    """Get a valid password within the range."""
     password = input('Enter a password between {} to {} characters: '.format(MIN_LENGTH, MAX_LENGTH))
-print('Password Created: {}'.format(len(password)*'*'))
+    while len(password) < MIN_LENGTH or len(password) > MAX_LENGTH:
+        print('Error: Password Does Not Meet Required Length')
+        password = input('Enter a password between {} to {} characters: '.format(MIN_LENGTH, MAX_LENGTH))
+    return password
+
+
+def print_asterisk(user_input):
+    """Print a number of asterisks."""
+    print('Password Created: {}'.format(len(user_input) * '*'))
+
+
+main()
