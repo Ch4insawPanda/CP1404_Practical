fe0e64d68320b752091ee7b26a8c723a594c248a
Josiah Teh <lingqiangjosiah.teh@my.jcu.edu.au>
2021-07-28 15:39:50 +0800

    Refactor the program to use functions instead.

--- prac_03/broken_score.py
+++ prac_03/broken_score.py
@@ -3,17 +3,27 @@ CP1404/CP5632 - Practical
 Broken program to determine score status
 """
 
-# TODO: Fix this!
 #               SCORE RANGE
 #       Bad       Passable            Excellent
 # 0<--------------->50<------------>90<------->100
-score = float(input("Enter score: "))
-if score < 0 or score > 100:   #Any score below 0 and above 100 is invalid
-    print("Invalid score")
-else:
-    if score >= 90:
-        print("Excellent")     #Excellent score range from 90 to 100
-    elif score >= 50:
-        print("Passable")      #Passable score range from 50 to 89
+
+
+def main():
+    score = float(input("Enter score: "))
+    result = get_result(score)
+    print(result)
+
+
+def get_result(score):
+    """Get the result of the user by use of the score."""
+    if score < 0 or score > 100:  # Any score below 0 and above 100 is invalid
+        return "Invalid score"
     else:
-        print("Bad")           #Bad score range from 0 to 49
+        if score >= 90:
+            return "Excellent"  # Excellent score range from 90 to 100
+        elif score >= 50:
+            return "Passable"   # Passable score range from 50 to 89
+        else:
+            return "Bad"        # Bad score range from 0 to 49
+
+main()
