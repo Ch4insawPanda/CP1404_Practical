a8c800ac8fcb7e82a4554ff746e24997dc18dab3
Josiah Teh <lingqiangjosiah.teh@my.jcu.edu.au>
2021-07-28 15:24:08 +0800

    Refactor the program to use functions instead

--- prac_03/temperatures.py
+++ prac_03/temperatures.py
@@ -3,25 +3,47 @@ CP1404/CP5632 - Practical
 Pseudocode for temperature conversion
 """
 
-MENU = """C - Convert Celsius to Fahrenheit
+def main():
+    menu()
+    choice = get_choice()
+    while choice != "Q":
+        if choice == "C":
+            convert_celsius()
+        elif choice == "F":
+            convert_fahrenheit()
+        else:
+            print("Invalid option")
+        menu()
+        choice = get_choice()
+    print("Thank you.")
+
+
+def menu():
+    """Print options for user."""
+    MENU = """C - Convert Celsius to Fahrenheit
 F - Convert Fahrenheit to Celsius
 Q - Quit"""
-print(MENU)
-choice = input(">>> ").upper()
-while choice != "Q":
-    if choice == "C":
-        celsius = float(input("Celsius: "))
-        fahrenheit = celsius * 9.0 / 5 + 32
-        print("Result: {:.2f} F".format(fahrenheit))
-    elif choice == "F":
-        # TODO: Write this section to convert F to C and display the result
-        # Hint: celsius = 5 / 9 * (fahrenheit - 32)
-        # Remove the "pass" statement when you are done. It's a placeholder.
-        fahrenheit = float(input("Fahrenheit: "))
-        celsius = 5 / 9 * (fahrenheit - 32)
-        print(f'Result: {celsius:.2f}')
-    else:
-        print("Invalid option")
     print(MENU)
+
+
+def convert_celsius():
+    """Convert celsius input into fahrenheit."""
+    celsius = float(input("Celsius: "))
+    fahrenheit = celsius * 9.0 / 5 + 32
+    print("Result: {:.2f} F".format(fahrenheit))
+
+
+def convert_fahrenheit():
+    """Convert fahrenheit input into celsius."""
+    fahrenheit = float(input("Fahrenheit: "))
+    celsius = 5 / 9 * (fahrenheit - 32)
+    print(f'Result: {celsius:.2f}')
+
+
+def get_choice():
+    """Get user choice."""
     choice = input(">>> ").upper()
-print("Thank you.")
+    return choice
+
+
+main()
