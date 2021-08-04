58dd80a2fff7e0b9ad6d897226f866a037abf900
Josiah Teh <lingqiangjosiah.teh@my.jcu.edu.au>
2021-08-04 17:03:14 +0800

    Added the password checker section

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -8,12 +8,14 @@ def main():
 
 
 def get_number():
+    '''Get a number of values depending on the total numbers.'''
     for number in range(TOTALNUMBERS):
         choice = int(input('Enter Number :'))
         numbers_list.append(choice)
 
 
 def number_display():
+    '''Display the different values from the users input'''
     print('The first number is {}.'.format(numbers_list[0]))
     print('The last number is {}.'.format(numbers_list[-1]))
     print('The smallest number is {}.'.format(min(numbers_list)))
@@ -22,3 +24,11 @@ def number_display():
 
 
 main()
+
+
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn', 'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+user_password = input('Enter Password :')
+if user_password in usernames:
+    print('Access Granted')
+else:
+    print('Access Denied')
